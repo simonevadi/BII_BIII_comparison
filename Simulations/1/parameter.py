1de4ae9865fd078c2f4bbfb7be890a4782ffb3b2
Sebastian Malerz <sebastian.malerz@helmholtz-berlin.de>
2024-07-05 17:44:29 +0200

    Simulation B2/B3/long/short/High_low-Beta

--- Simulations/1/parameter.py
+++ Simulations/1/parameter.py
@@ -1,22 +1,26 @@
 import numpy as np
 
-rml_file_name_b3= 'bessy3'
-rml_file_name_b2= 'bessy2'
+rml_file_name_bessy3_short          = 'Bessy3_short'
+rml_file_name_bessy3_long           = 'Bessy3_long'
+rml_file_name_bessy2_HiBeta_short   = 'Bessy2_HiBeta_short'
+rml_file_name_bessy2_HiBeta_long    = 'Bessy2_HiBeta_long'
+rml_file_name_bessy2_LoBeta_short   = 'Bessy2_LoBeta_short'
+rml_file_name_bessy2_LoBeta_long    = 'Bessy2_LoBeta_long'
 
 
 order       = 1
-SlitSize    = np.array([ .05, .01])
+SlitSize    = np.array([ .1, .05, .01])
 grating     = np.array([2400])
 cff         = np.array([2.25])
 
 energy_flux = np.arange(100, 2001,50)
 energy_rp   = np.arange(100, 2001,50)
 
-nrays_flux  = 1e5
-nrays_rp    = 1e5
+nrays_flux  = 1e4
+nrays_rp    = 1e4
 
-round_flux = 1
-round_rp   = 1
+round_flux = 2
+round_rp   = 2
 
 
 ncpu = 10
