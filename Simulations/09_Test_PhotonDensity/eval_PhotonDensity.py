95c28d82d06e518195406848204963e73778bc62
Simone Vadilonga <simone.vadilonga@helmholtz-berlin.de>
2025-01-15 15:48:40 +0100

    update

--- Simulations/09_Test_PhotonDensity/eval_PhotonDensity.py
+++ Simulations/09_Test_PhotonDensity/eval_PhotonDensity.py
@@ -5,7 +5,7 @@ import pandas as pd
 # Read CSV-File
 
 simdata_B2lo = pd.read_csv('RAYPy_Simulation_bessy2_LoBeta_long_52_FLUX/DetectorAtFocus_RawRaysOutgoing.csv')
-simdata_B2hi = pd.read_csv('RAYPy_Simulation_bessy2_HiBeta_long_52_FLUX/DetectorAtFocus_RawRaysOutgoing.csv')
+simdata_B2hi = pd.read_csv('RAYPy_Simulation_Bessy2_HiBeta_long_52_FLUX/DetectorAtFocus_RawRaysOutgoing.csv')
 simdata_B3= pd.read_csv('RAYPy_Simulation_bessy3_long_52_FLUX/DetectorAtFocus_RawRaysOutgoing.csv')
 
 fig, (ax) = plt.subplots(1, 1,figsize=(15,10))
@@ -13,6 +13,8 @@ fig, (ax) = plt.subplots(1, 1,figsize=(15,10))
 
 ExitSlit_list = simdata_B2lo['ExitSlit.openingHeight'].unique()
 for ExitSlit in ExitSlit_list:
+    if ExitSlit != 0.015:
+        continue
     reduced_simdata_B2lo = simdata_B2lo[simdata_B2lo['ExitSlit.openingHeight']==ExitSlit]
     energy_B2lo = reduced_simdata_B2lo['SU.photonEnergy']
     N_rays_B2lo = reduced_simdata_B2lo['PercentageRaysSurvived']
@@ -35,16 +37,16 @@ for ExitSlit in ExitSlit_list:
     PhotonDensity_B3 = N_rays_B3/((vf_B3*hf_B3)*1000)
 
     # Density Plot
-    lines, = ax.plot(energy_B2lo, PhotonDensity_B2lo, label = f'B2 low beta ExitSlit {int(ExitSlit*1000)}'+' µm', marker='s')
+    lines, = ax.plot(energy_B2hi, PhotonDensity_B2hi,  label = f'B2 high beta ExitSlit {int(ExitSlit*1000)}'+' µm')
     used_colors = lines.get_color()
-    ax.plot(energy_B2hi, PhotonDensity_B2hi, color = 'green', label = f'B2 high beta ExitSlit {int(ExitSlit*1000)}'+' µm', marker='+')
-    ax.plot(energy_B3, PhotonDensity_B3, color = 'red', label = f'B3 ExitSlit {int(ExitSlit*1000)}'+' µm', marker='o')
+    # ax.plot(energy_B2lo, PhotonDensity_B2lo,color = used_colors, label = f'B2 low beta ExitSlit {int(ExitSlit*1000)}'+' µm', marker='s')
 
+    ax.plot(energy_B3, PhotonDensity_B3, color = used_colors, label = f'B3 ExitSlit {int(ExitSlit*1000)}'+' µm', linestyle='dashed')
 
 ax.minorticks_on()
 ax.grid(linestyle='dotted', color='grey')
 ax.tick_params(axis='both', labelsize=14)
-ax.set_ylabel ('Photon density [%]', fontsize=14)
+ax.set_ylabel ('Photon density [%/µm²]', fontsize=14)
 # ax.set_ylabel(r'$\frac{N}{\mathrm{[µm^2]}} \, @ \, 0.1\% \, \mathrm{bandwidth}$' + ' in [%]', fontsize=16)
 ax.set_xlabel('Energy [eV]', fontsize= 16)
 ax.set_title('BESSY II (LowBeta) vs. BESSY III @ 56 m standard PGM-BL PhotonDensity', fontsize= 18)
